0309d46a7acc7fe620d75ea5350cad1d1a289b53
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2024-10-09 21:11:02 +0200

    Adición de ejercicios
    Adición de ejercicios y finalización de anteriores

--- UD04 funciones/script.js
+++ UD04 funciones/script.js
@@ -273,7 +273,6 @@ console.log(ordenarNumeros(8, 2, 5, 1)); // Salida: [1, 2, 5, 8]
 
 
 
-
     // Ejercicio recursividad:
 /*  Podemos usar % para verificar si un número es par o impar
     al usar el %2 para ver si es divisible por dos.
@@ -289,16 +288,132 @@ console.log(ordenarNumeros(8, 2, 5, 1)); // Salida: [1, 2, 5, 8]
 
 
 
-function esPar(num) {
-    if (num == 0) {
-        return true; // 0 es par
-    } else if (num == 1) {
-        return false; // 1 es impar
+// function esPar(num) {
+//     if (num == 0) {
+//         return true; // 0 es par
+//     } else if (num == 1) {
+//         return false; // 1 es impar
+//     } else {
+//         return esPar(num - 2); // Restar 2 y llamar recursivamente
+//     }
+// }
+
+// // Prueba la función
+// console.log(esPar(50)); // Salida: true
+// console.log(esPar(51)); // Salida: false
+
+
+
+//https://github.com/jmelsiondwec/dwec24-25
+
+
+
+
+/*
+Se conoce como constante de Kaprekar (en honor al matemático D. R. Kaprekar) 
+al punto fijo de la aplicación iterativa de la denominada Operación de Kaprekar,
+1​2​ que consiste en calcular la diferencia entre un número cualquiera con sus 
+dígitos ordenados de mayor a menor y dicho número con el orden de sus dígitos 
+de menor a mayor.
+
+La constante de Kaprekar más conocida es el 6174, que ocurre cuando el número 
+inicial de la operación de Kaprekar tiene cuatro dígitos y está en base 10. 
+Por ejemplo, con el número 2435:
+
+    Se comienza con el número 2435 {\displaystyle 2435}.
+    Se ordenan sus dígitos en orden descendente: 5432 {\displaystyle 5432}.
+    Se ordenan sus dígitos en orden ascendente: 2345 {\displaystyle 2345}.
+    Se resta el número ascendente al número descendente: 5432 − 2345 = 3087 
+    {\displaystyle 5432-2345=3087}.
+    Se repite el proceso con el resultado obtenido: 8730 − 0378 = 8352 
+    {\displaystyle 8730-0378=8352}.
+    Se continúa repitiendo el proceso: 8532 − 2358 = 6174 {\displaystyle 8532-2358=6174}.
+    Una vez que se llega a 6174, la operación se detiene: 7641 − 1467 = 6174 
+    {\displaystyle 7641-1467=6174}.
+
+Como aplicar la operación de Kaprekar a 6174 da 6174, ese número es un punto fijo; 
+y por tanto, una constante de Kaprekar
+
+El cero es una constante de Kaprekar trivial para todos los casos, debido a 
+que 0 − 0 = 0 {\displaystyle 0-0=0}. Lógicamente, si el número que se selecciona 
+tiene todas sus cifras iguales, llevará a la constante trivial. 
+*/
+
+
+
+// function kaprekar(n1, n2, n3, n4) {
+
+
+//     let resultado = ordenarNumeros(n1, n2, n3, n4);
+//     let asc = [resultado[0]] + [resultado[1]] + [resultado[2]] + [resultado[3]];
+//     let desc = [resultado[3]] + [resultado[2]] + [resultado[1]] + [resultado[0]];
+
+//     let resta =desc - asc;
+
+//     if (resta < 1000) {
+//         resta = parseInt(String(resta).padStart(4, '0'));
+//     }
+
+//     let restaArray = Array.from(String(resta), Number);
+
+//     console.log(restaArray);
+
+//     if (resta == 6174) {
+//         return console.log("finalizado");
+//     }else {
+//         //return kaprekar(resta[0], resta[1], resta[2], resta[3]);
+//     }
+    
+// }
+
+
+function kaprekar(n1, n2, n3, n4, contador = 0) {
+    let resultado = ordenarNumeros(n1, n2, n3, n4);
+
+    // Formar los números ascendente y descendente
+    let asc = parseInt(resultado.join(''));
+    let desc = parseInt(resultado.reverse().join(''));
+
+    let resta = desc - asc;
+
+    // Asegúrate de que 'resta' sea un número válido
+    if (resta < 1000) {
+        resta = parseInt(String(resta).padStart(4, '0'));
+    }
+
+    let restaArray = Array.from(String(resta), Number);
+
+    console.log(`Iteración ${contador}:`, restaArray);
+
+    if (resta === 6174) {
+        console.log("finalizado en la iteración", contador);
     } else {
-        return esPar(num - 2); // Restar 2 y llamar recursivamente
+        // Llamada recursiva con los dígitos de 'resta'
+        return kaprekar(...restaArray, contador + 1);
     }
 }
 
-// Prueba la función
-console.log(esPar(50)); // Salida: true
-console.log(esPar(51)); // Salida: false
+
+
+
+
+
+kaprekar(6,1,7,4);
+
+
+
+
+
+function contarPeces(cadena, posicionAnterior, contador = 0) {
+    let siguentePez = cadena.indexOf("><>", posicionAnterior);
+
+    if (siguentePez == -1) {
+        return console.log("Se encontraron " + contador + " peces");
+    }else {
+        return contarPeces(cadena, siguentePez + 3, contador + 1);
+    }
+}
+
+
+let mar = "~~~><>~~~><>><>~~~><>~~~~~~><>~><>~~~~><>~~~><>><>~~~><>~~~~~~><>~><>~";
+contarPeces(mar);
