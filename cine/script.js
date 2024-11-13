2e57d3116af653df6e7a32aaf6ac03dc1cd057a3
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2024-11-13 21:12:44 +0100

    final cine

--- cine/script.js
+++ cine/script.js
@@ -7,14 +7,73 @@ const peliculaSelect = document.getElementById('pelicula');
 
 
 //precio de la pelicula seleccionada
-let precioTicket = peliculaSelect.value;
+let precioTicket = +peliculaSelect.value;
 
-console.log(precioTicket);
+llenaIU();
+
+
+//functiones
+function actualizaSeleccionAsientos (){
+    const asientosSeleccionados = document.querySelectorAll('.fila .asiento.seleccionado');
+
+    //función normal
+    // const indiceAsientos = [...asientosSeleccionados].map(function(asiento){
+    //     return [...asientos].indexOf(asiento);
+    // });
+
+    //función flecha
+    const indiceAsientos = [...asientosSeleccionados].map((asiento)=>[...asientos].indexOf(asiento));
+
+    localStorage.setItem('asientosSeleccionados', JSON.stringify(indiceAsientos));
+
+    const contadorAsientosSeleccionados = asientosSeleccionados.length;
+
+    contador.innerText = contadorAsientosSeleccionados;
+
+    total.innerText = contadorAsientosSeleccionados * precioTicket;
+}
+
+function llenaIU() {
+    const asientosSeleccionados = JSON.parse(localStorage.getItem('asientosSeleccionados'));
+
+    if(asientosSeleccionados !== null && asientosSeleccionados.length > 0){
+
+        asientos.forEach((asiento, index)=>{
+            if(asientosSeleccionados.indexOf(index) > -1){
+                asiento.classList.add('seleccionado');
+            }
+        });
+    }
+
+    const indicePeliculaSeleccionada = localStorage.getItem('indicePeliculaSeleccionada');
+
+    if(indicePeliculaSeleccionada){
+        peliculaSelect.selectedIndex = indicePeliculaSeleccionada;
+    }
+
+    const precioPeliculaSeleccionada = localStorage.getItem('precioPeliculaSeleccionada');
+
+    if(precioPeliculaSeleccionada){
+        precioTicket = +precioPeliculaSeleccionada;
+    }
+}
 
 
 //eventos
 contenedor.addEventListener('click', (e)=>{
     if(e.target.classList.contains('asiento') && !e.target.classList.contains('ocupado')){
-        console.log('hola');
+        e.target.classList.toggle('seleccionado');
+        actualizaSeleccionAsientos();
     }
 });
+
+peliculaSelect.addEventListener('change', (e)=>{
+    precioTicket = +e.target.value;
+
+    localStorage.setItem('indicePeliculaSeleccionada', e.target.selectedIndex);
+    localStorage.setItem('precioPeliculaSeleccionada', e.target.value);
+
+    actualizaSeleccionAsientos();
+});
+
+actualizaSeleccionAsientos();
