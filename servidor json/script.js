85aa0b3b50eb57e7ec39362aa609bcee897a103e
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2024-12-18 21:06:49 +0100

    Continuación

--- servidor json/script.js
+++ servidor json/script.js
@@ -3,20 +3,27 @@ const urlPalabras = "http://localhost:3000/palabras";
 const url = window.location.pathname;
 if (url.includes("palabras-list.html")) {
     cargarPalabras();
-
-};
+} else if (url.includes("palabras-edit.html")) {
+    const parametros = new URLSearchParams(window.location.search);
+    const palabraId = parametros.get("id");
+    if (palabraId) {
+        cargarPalabra(palabraId);
+    }
+}
 
 document.getElementById("palabra-form")?.addEventListener("submit", guardarPalabra)
 
 async function guardarPalabra(e) {
     e.preventDefault();
 
-    const metodo = "POST";
+    const id = new URLSearchParams(window.location.search).get("id");
 
-    const url = urlPalabras;
+    const methodo = id ? "PUT" : "POST";
+
+    const urlGuardar = id ? `${urlPalabras}/${id}` : urlPalabras;
 
     const palabra = {
-        id: 0,
+        id: "0",
         palabra: document.getElementById("palabra").value,
         dificultad: +document.getElementById("dificultad").value
     }
@@ -25,12 +32,13 @@ async function guardarPalabra(e) {
         .then((response) => response.json())
         .then((data) => {
             if (data && data.length > 0) {
-                palabra.id = data[data.length - 1].id + 1;
+                let idAux = +data[data.length - 1].id + 1;
+                palabra.id = idAux + "";
             } else {
-                palabra.id = 1;
+                palabra.id = "1";
             }
 
-            return fetch(`${url}`, {
+            return fetch(`${urlGuardar}`, {
                 method: metodo,
                 body: JSON.stringify(palabra),
                 headers: {
@@ -38,8 +46,8 @@ async function guardarPalabra(e) {
                 }
             });
         })
-        .then(()=>window.location.href = "palabras-list.html")
-        .catch((error)=>console.log("Error al guardar la palabra: ", error));
+        .then(() => window.location.href = "palabras-list.html")
+        .catch((error) => console.log("Error al guardar la palabra: ", error));
 }
 
 async function cargarPalabras() {
@@ -48,11 +56,11 @@ async function cargarPalabras() {
         const palabras = await response.json();
         const listadoPalabras = document.getElementById("listado-palabras");
 
-        if(palabras.length === 0) {
+        if (palabras.length === 0) {
             listadoPalabras.innerHTML = "<p>No hay palabras registradas.</p>";
         } else {
-            listadoPalabras.innerHTML = palabras.map((palabra)=>
-                `<div class="palabra>
+            listadoPalabras.innerHTML = palabras.map((palabra) =>
+                `<div class="palabra">
                     <p>${palabra.palabra} - ${palabra.dificultad}</p>
                     <button class="eliminar" onclick="eliminarPalabra(${palabra.id})">Eliminar</button>
                     <button class="editar" onclick="editarPalabra(${palabra.id})">Editar</button>
@@ -61,5 +69,37 @@ async function cargarPalabras() {
         }
     } catch (error) {
         console.log("Error al cargar las palabras: ", error);
-    }    
+    }
+}
+
+
+async function cargarPalabra(id) {
+    fetch(urlPalabras)
+        .then((response) => response.json())
+        .then((data) => {
+            const palabra = data.find((palabraP) => palabraP.id == id);
+            document.getElementById("palabra").value = palabra.palabra;
+            document.getElementById("dificultad").value = palabra.dificultad;
+        })
+        .catch((error)=>console.log("error al cargar la palabra: ", error));
+}
+
+
+async function eliminarPalabra(id) {
+    const urlDelete = `${urlPalabras}/${id}`;
+
+    fetch(`${urlDelete}/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then(() => cargarPalabras())
+        .catch((error) => console.log("Error al eliminar las palabras: ", error))
+}
+
+
+async function editarPalabra(id) {
+    window.location.href = `palabras-edit.html?id=${id}`
 }
