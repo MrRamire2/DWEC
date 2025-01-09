17f823943b1788b45ccd7d63e6389a12149647af
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2025-01-09 19:37:35 +0100

    Update App.js

--- react/mi-primera-react-app/src/App.js
+++ react/mi-primera-react-app/src/App.js
@@ -1,26 +1,67 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+//HOOK
+//useState
 
 function App() {
-  const titulo = 'Bienvenidos a mi componente App';
-  const paginas = 20;
-  const clase = {
-    nombre: 'DAW',
-    curso: '24-25',
-    alumnos: 10
-  }
+  const [mostrarEventos, setMostrarEventos] = useState(true);
+
+  const [eventos, setEventos] = useState([
+    { titulo: "Examen DWEC", id: 1 },
+    { titulo: "Curso Programación", id: 2 },
+    { titulo: "Fiesta de Sant Antoni", id: 3 },
+  ]);
+
+  console.log(mostrarEventos);
+
+  const handleClick = (id) => {
+    //version corta
+    setEventos((eventosPrevios) =>
+      eventosPrevios.filter((evento) => id !== evento.id)
+    );
+
+    //version larga
+    // setEventos((eventosPrevios)=>{
+    //   return eventosPrevios.filter((evento)=>{
+    //     return id !== evento.id;
+    //   })
+    // });
+
+    // setEventos(eventos.filter((evento)=>{
+    //   return id !== evento.id;
+    // }))
+
+    console.log(id);
+  };
 
   return (
     <div className="App">
-      <div className="contenido">
-        <h1>{titulo}</h1>
-        <p>{paginas}</p>
-        <p>{[1,2,3,4,5,6]}</p>
-        <p>Nombre de la clase: {clase.nombre}</p>
-        <p>Numero aleatorio {Math.random() *1000}</p>
-        <p>Fecha: {new Date().toDateString()}</p>
-      </div>
+      {mostrarEventos && (
+        <div>
+          <button onClick={() => setMostrarEventos(false)}>Ocultar eventos</button>
+        </div>
+      )}
+      {!mostrarEventos && (
+        <div>
+          <button onClick={() => setMostrarEventos(true)}>Mostrar eventos</button>
+        </div>
+      )}
+      {mostrarEventos && eventos.map((evento, index) => (
+        <div key={evento.id}>
+          <h2>
+            {index} - {evento.titulo}
+          </h2>
+          <button onClick={() => handleClick(evento.id)}>
+            Eliminar evento
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+
+
