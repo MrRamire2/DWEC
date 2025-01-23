4d5421c46c1eab27db75883d6396a9a080e1cc91
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2025-01-23 19:44:25 +0100

    asignaturas

--- react/dawb/src/components/AsignaturasLista.js
+++ react/dawb/src/components/AsignaturasLista.js
@@ -1,31 +1,28 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import { useFetch } from "../hooks/useFetch";
 import "./AsignaturasLista.css"
 
 
 export default function AsignaturasLista() {
-   
-    const[asignaturas, setAsignaturas] = useState([]);
+    const [url, setUrl] = useState('http://localhost:3000/asignaturas');
 
-    console.log(asignaturas)
-
-    useEffect(()=>{
-        fetch('http://localhost:3000/asignaturas')
-        .then(response => response.json())
-        .then(json => setAsignaturas(json))
-    }, []);
-    //Array de dependencias.
+const {datos: asignaturas} = useFetch(url);
 
   return (
     <div className="asignatura-lista">
         <h2>Listado de Asignaturas</h2>
         <ul>
-            {asignaturas.map(asignatura => (
+            {asignaturas && asignaturas.map(asignatura => (
                 <li key={asignatura.id}>
                     <h3>{asignatura.nombre}</h3>
                     <p>Horas Totales: {asignatura["horas totales"]}</p>
                 </li>
             ))}
         </ul>
+        <div className="filtros">
+            <button onClick={()=>setUrl('http://localhost:3000/asignaturas?idioma=Ingles')}>Inglés</button>
+            <button onClick={()=>setUrl('http://localhost:3000/asignaturas')}>Todos</button>
+        </div>
     </div>
   )
 }
