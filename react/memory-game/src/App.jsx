b5b63423ed7be204542df61d33833338ab1ccb1f
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2025-02-05 20:13:36 +0100

    cartas

--- react/memory-game/src/App.jsx
+++ react/memory-game/src/App.jsx
@@ -1,46 +1,90 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import Carta from './components/Carta';
 
 function App() {
 
   const [cartas, setCartas] = useState([]);
+  const [eleccion1, setEleccion1] = useState(null);
+  const [eleccion2, setEleccion2] = useState(null);
+  const [deshabilitado, setDeshabilitado] = useState(false);
+  const [turnos, setTurnos] = useState(0);
+
+  const imagenes = [
+    { "src": "/img/1.png", emparejada: false },
+    { "src": "/img/2.png", emparejada: false },
+    { "src": "/img/3.png", emparejada: false },
+    { "src": "/img/4.png", emparejada: false },
+    { "src": "/img/5.png", emparejada: false },
+    { "src": "/img/6.png", emparejada: false }
+  ];
 
   const mezclaCartas = () => {
     const cartasMezcladas = [...imagenes, ...imagenes]
       .sort(() => Math.random() - 0.5)
       .map((carta) => ({ ...carta, id: Math.floor(Math.random() * 10000) }));
 
-    setCartas(cartasMezcladas);
-
+      setEleccion1(null);
+      setEleccion2(null);
+      setTurnos(0);
+      setCartas(cartasMezcladas);
   }
 
-  console.log(cartas);
+  const handleEleccion = (carta) => {
+    eleccion1 ? setEleccion2(carta) : setEleccion1(carta);
+  }
 
-  const imagenes = [
-    { "src": "/img/1.png" },
-    { "src": "/img/2.png" },
-    { "src": "/img/3.png" },
-    { "src": "/img/4.png" },
-    { "src": "/img/5.png" },
-    { "src": "/img/6.png" },
-  ];
-  
+  useEffect(()=>{
+    if (eleccion1 && eleccion2) {
+      setDeshabilitado(true);
+      if (eleccion1.src === eleccion2.src) {
+      setCartas(cartasPrevias => {
+          return cartasPrevias.map(carta => {
+            if (carta.src === eleccion1.src) {
+              return {...carta, emparejada: true};
+            } else {
+              return carta;
+            }
+          });
+        });
+        reseteaTurno();
+      } else {
+        setTimeout(()=>reseteaTurno(), 1000);
+      }
+    }
+  }, [eleccion1, eleccion2]);
+
+  useEffect(() => {
+    mezclaCartas();
+  }, []);
+
+  const reseteaTurno = () => {
+    setEleccion1(null);
+    setEleccion2(null);
+    setDeshabilitado(false);
+    setTurnos(turnosPrevios => turnosPrevios + 1);
+  } 
 
   return (
     <div className='App'>
       <h1>Joc de Memoria</h1>
-      <button onClick={mezclaCartas}>Mezclar</button>
+      <button onClick={mezclaCartas}>Mezclar Cartas</button>
 
       <div className='card-grid'>
 
         {
           cartas.map(carta => (
-            <Carta carta={carta} key={carta.id} />
+            <Carta carta={carta}
+            key={carta.id}
+            handleEleccion = {handleEleccion}
+            girada = {carta===eleccion1 || carta===eleccion2 || carta.emparejada} 
+            deshabilitado = {deshabilitado}
+            />
           ))
         }
 
       </div>
+      <p>Turnos: {turnos}</p>
     </div>
   )
 }
--- react/memory-game/src/components/Carta.jsx
+++ react/memory-game/src/components/Carta.jsx
@@ -1,11 +1,18 @@
 import './Carta.css';
 
-export default function Carta({ carta }) {
+export default function Carta({ carta, handleEleccion, girada, deshabilitado }) {
+    
+    const handleClick = () => {
+        if (!deshabilitado) {
+            handleEleccion(carta);
+        }
+    }
+
     return (
         <div className='card'>
-            <div>
-                <img src={carta.src} alt="Frontal" />
-                <img src="./img/atras.png" alt="Cubierta" />
+            <div className={girada ? "flipped" : ""}>
+                <img className='front' src={carta.src} alt="Frontal" />
+                <img className='back' src="./img/cubierta.webp" onClick={handleClick} alt="Cubierta" />
             </div>
         </div>
     )
