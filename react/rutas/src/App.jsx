41e4ed9ce1a7b445883b62481d14143472611b8d
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2025-02-12 20:15:50 +0100

    Receptas

--- react/rutas/src/App.jsx
+++ react/rutas/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import Inici from './pages/Inici';
 import Cuiners from './pages/Cuiners';
 import Plats from './pages/Plats';
+import Recepta from './pages/Recepta';
 
 
 
@@ -13,7 +14,7 @@ function App() {
       <BrowserRouter>
         <nav>
           <h1>Les Meves Receptes</h1>
-          <NavLink to="/">Inici</NavLink>
+          <NavLink exact to="/">Inici</NavLink>
           <NavLink to="/Cuiners">Cuiners</NavLink>
           <NavLink to="/Plats">Plats</NavLink>
         </nav>
@@ -26,6 +27,9 @@ function App() {
           </Route>
           <Route path="/Plats">
             <Plats />
+          </Route>
+          <Route path="/receptes/:id">
+            <Recepta />
           </Route>
         </Switch>
       </BrowserRouter>
--- react/rutas/src/pages/Inici.jsx
+++ react/rutas/src/pages/Inici.jsx
@@ -1,8 +1,23 @@
+import { useFetch } from "../hooks/useFetch"
+import { Link } from 'react-router-dom';
+import './Inici.css';
+
 export default function Inici() {
+
+  const { datos:receptes, cargando, error } = useFetch('http://localhost:3000/receptes');
+
   return (
-    <div>
-      <h2>Inici</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quaerat, tempore animi omnis, nesciunt, cupiditate eveniet enim asperiores iste nemo architecto sunt aspernatur! Obcaecati expedita possimus omnis temporibus, voluptatum eveniet!</p>
+    <div className="inici">
+      <h2>Receptes</h2>
+      {cargando && <div>Cargando</div>}
+      {error && <div>{error}</div>}
+      { receptes && receptes.map(recepta => (
+        <div key={recepta.id} className='card'>
+          <h3>{recepta.nom}</h3>
+          <p>{recepta.autor}</p>
+          <Link to={`/receptes/${recepta.id}`}>Ir al detalle de la receta...</Link>
+        </div>
+      ))}
     </div>
   )
 }
